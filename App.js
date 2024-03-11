b2d893c40aaa24b955e57c35604dc55215a3968a
Janice-M <janiceink001@gmail.com>
2024-03-11 17:44:29 +0300

    mama bushra innit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Helo mama  bushra </Text>
+      <Text>Hello mama  bushra </Text>
       <StatusBar style="auto" />
     </View>
   );
